9f099dee1c7cee96cdcfcab498edca6c1c1bfcbf
take-p <merry3nosheep@gmail.com>
2025-04-05 21:18:47 +0900

    feat(example): add example app with asset audio playback demo

---
+++ example/android/app/src/main/kotlin/com/example/audio_kit_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.audio_kit_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ example/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:audio_kit/audio_kit.dart';
+import 'package:flutter/material.dart';
+
+final sourceMap = {
+  'bgm_1': AssetAudioSource('assets/bgm/bgm_1_fixed.mp3'),
+  'bgm_2': AssetAudioSource('assets/bgm/bgm_2.mp3'),
+  'se_click': AssetAudioSource('assets/se/se.mp3'),
+};
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final audioKit = AudioKit();
+
+  // オーディオソースの登録（Asset 使用）
+  await audioKit.registerAll(sourceMap);
+
+  runApp(MyApp(audioKit: audioKit));
+}
+
+class MyApp extends StatelessWidget {
+  final AudioKit audioKit;
+
+  const MyApp({super.key, required this.audioKit});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'audio_kit Example',
+      home: AudioDemoScreen(audioKit: audioKit),
+    );
+  }
+}
+
+class AudioDemoScreen extends StatelessWidget {
+  final AudioKit audioKit;
+
+  const AudioDemoScreen({super.key, required this.audioKit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('audio_kit Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await audioKit.play(key: 'se_click');
+              },
+              child: const Text('Play SE'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                await audioKit.play(key: 'bgm_1', channel: 'bgm', loop: true);
+              },
+              child: const Text('Play BGM1'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                await audioKit.play(
+                  key: 'bgm_2',
+                  channel: 'bgm',
+                  loop: true,
+                  // loopStartPosition: Duration(milliseconds: 666),
+                  loopStartPosition: Duration(seconds: 3),
+                );
+              },
+              child: const Text('Play BGM2'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                await audioKit.stop(channel: 'bgm');
+              },
+              child: const Text('Stop BGM'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
